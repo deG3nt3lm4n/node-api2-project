7cbbf4b35e705c5dfe31318bd8cdbef293a0e0bc
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-04-06 16:07:57 -0400

    :sparkles: server up

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('*', (req,res) => {
+
+  res.send('<h1>What is life?</h1>')
+
+})
+
+module.exports = server
