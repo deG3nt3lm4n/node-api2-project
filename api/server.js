24cab26fa7dfbaa8a375d089708a2924d5d2f2cf
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-04-07 11:34:30 -0400

    :sparkles: user gets the posts-needs styles and adding and delteing

--- api/server.js
+++ api/server.js
@@ -1,17 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express')
-const postRouter = require('./posts/posts-router')
+const cors = require('cors')
 
+const postRouter = require('./posts/posts-router')
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 
 server.use('/api/posts', postRouter)
 
 // Mini little thingy
-server.get('*', (req,res) => {
-  res.send('<h1>What is life?</h1>')
-})
+// server.get('*', (req,res) => {
+//   res.send('<h1>What is life?</h1>')
+// })
 
 module.exports = server
--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server')
 const PORT = 3001
 
 server.listen(PORT, () => {
-  console.log('Server up and running on ', PORT)
+  console.log('Server up and running on', PORT)
 })
